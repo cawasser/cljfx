11f6910f869a6e682a8e3c5091aa3761955a8256
Chris Wasser <cawasser@mac.com>
2023-08-29 00:57:45 -0400

    - added some limited state (row data) - added width-related props

--- examples/e100_table_view.clj
+++ examples/e100_table_view.clj
@@ -13,7 +13,7 @@
 
 
 (def reg {:cell/string #'string-cell
-          :cell/color #'color-cell})
+          :cell/color  #'color-cell})
 
 
 (def column-defs [{:column/id     :name
@@ -22,27 +22,41 @@
                   {:column/id     :color
                    :column/name   "Color"
                    :column/render :cell/color}
-                  {:column/id     :description
-                   :column/name   "Description"
-                   :column/render :cell/string}])
+                  {:column/id         :description
+                   :column/min-width  50
+                   :column/pref-width 200
+                   :column/max-width  300
+                   :column/name       "Description"
+                   :column/render     :cell/string}])
 
 
-(def data [{:name "red" :color :red :description "This is red"}
-           {:name "green" :color :green :description "This is green"}
-           {:name "blue" :color :blue :description "This is blue"}
-           {:name "yellow" :color :yellow :description "This is yellow"}
-           {:name "cyan" :color :cyan :description "This is cyan"}])
+(def default-data [{:name "red" :color :red :description "This is red"}
+                   {:name "green" :color :green :description "This is green"}
+                   {:name "blue" :color :blue :description "This is blue"}
+                   {:name "yellow" :color :yellow :description "This is yellow"}
+                   {:name "cyan" :color :cyan :description "This is cyan"}])
 
 
-(def app-db (atom data))
+(def app-db (atom {:data default-data}))
 
 
-(defn header-cell [{:keys [column/name column/render] :as cell-heading}]
-  {:fx/type            :table-column
-   :text               name
-   :cell-value-factory identity
-   :cell-factory       {:fx/cell-type :table-cell
-                        :describe     ((render reg) cell-heading)}})
+(defn- add-row [color-name color-label description]
+  (swap! app-db assoc :data
+    (conj (:data @app-db)
+      {:name color-label :color color-name :description description})))
+
+
+(defn header-cell [{:keys [column/name column/render
+                           column/min-width column/max-width column/pref-width]
+                    :as   cell-heading}]
+  (merge {:fx/type            :table-column
+          :text               name
+          :cell-value-factory identity
+          :cell-factory       {:fx/cell-type :table-cell
+                               :describe     ((render reg) cell-heading)}}
+    (when min-width {:min-width min-width})
+    (when max-width {:max-width max-width})
+    (when pref-width {:pref-width pref-width})))
 
 
 (defn table-view [{:keys [data columns pref-width pref-height]}]
@@ -53,22 +67,44 @@
    :items       data})
 
 
-(fx/on-fx-thread
-  (fx/create-component
-    {:fx/type :stage
-     :showing true
-     :title   "Table View example"
-     :scene   {:fx/type :scene
-               :root    {:fx/type     table-view
-                         :pref-width  960
-                         :pref-height 540
-                         :columns     column-defs
-                         :data        data}}}))
+
+(defn root [{:keys [data]}]
+  {:fx/type :stage
+   :showing true
+   :title   "Table View example"
+   :scene   {:fx/type :scene
+             :root    {:fx/type     table-view
+                       :pref-width  960
+                       :pref-height 540
+                       :columns     column-defs
+                       :data        data}}})
+
+
+(def renderer
+  (fx/create-renderer
+    :middleware (fx/wrap-map-desc assoc :fx/type root)))
+
+
+(fx/mount-renderer app-db renderer)
 
 
 
 
 
 (comment
+  (map header-cell column-defs)
+
+
+
+  ())
+
+
+(comment
+
+  (add-row :violet "violet" "This is violet")
+  (add-row :slateblue "slate blue" "This is slate blue")
+
+
+
 
   ())
