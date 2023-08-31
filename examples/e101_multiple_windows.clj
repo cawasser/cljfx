2169f9504c0ba742f1411c28109597f6a969884a
Chris Wasser <cawasser@mac.com>
2023-08-30 21:15:15 -0400

    - e101 table-view content type. Now to get re-frame like support, so we can subscribe to data depp down in the component tree

--- examples/e101_multiple_windows.clj
+++ examples/e101_multiple_windows.clj
@@ -5,12 +5,29 @@
   (:import [javafx.stage Screen]))
 
 
-(def app-db (atom {:description "some new text"
-                   :color       :yellow}))
+(def app-db (atom {:description            "some new text"
+                   :color                  :yellow
+                   :customer/orders        [{:name "red" :color :red :description "This is red"}
+                                            {:name "green" :color :green :description "This is green"}
+                                            {:name "blue" :color :blue :description "This is blue"}
+                                            {:name "yellow" :color :yellow :description "This is yellow"}
+                                            {:name "cyan" :color :cyan :description "This is cyan"}]
+                   :customer/order-columns [{:column/id     :name
+                                             :column/name   "Name"
+                                             :column/render :cell/string}
+                                            {:column/id     :color
+                                             :column/name   "Color"
+                                             :column/render :cell/color}
+                                            {:column/id         :description
+                                             :column/min-width  50
+                                             :column/pref-width 200
+                                             :column/max-width  300
+                                             :column/name       "Description"
+                                             :column/render     :cell/string}]}))
 
 
-(def width 300)
-(def height 100)
+(def width 600)
+(def height 300)
 
 (def bottom
   (-> (Screen/getPrimary)
@@ -25,29 +42,34 @@
     (- width)))
 
 
-(defn description [{:keys [description]}]
-  {:fx/type s/text-content
-   :x 0 :y 0 :width 0 :height 0
-   :description description})
 
-
-(defn root [{:keys [description color]}]
+(defn root [{:keys [description color
+                    customer/orders customer/order-columns]}]
   {:fx/type fx/ext-many
-   :desc    [{:fx/type w/window
-              :x       0
-              :y       0
-              :width   width
-              :height  height
-              ;:description description
-              :content {:fx/type description
-                        :description description}}                         ;s/text-content}
+   :desc    [{:fx/type     w/window
+              :x           0
+              :y           0
+              :width       width
+              :height      height
+              :description description
+              :content     s/text-content}                  ;s/text-content}
              {:fx/type w/window
               :x       400
               :y       0
               :width   width
               :height  height
               :color   color
-              :content s/color-content}]})
+              :content s/color-content}
+             {:fx/type     w/window
+              :x           800
+              :y           0
+              :width       width
+              :height      height
+              :pref-width  width
+              :pref-height height
+              :data        orders
+              :columns     order-columns
+              :content     s/table-view}]})
 
 
 (def renderer
--- examples/e101_support.clj
+++ examples/e101_support.clj
@@ -1,6 +1,10 @@
 (ns e101-support)
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; region ; TEXT CONTENT
+;
 (defn text-content [{:keys [x y width height description] :as params}]
   (println "text-content" params)
   {:fx/type :v-box
@@ -12,9 +16,57 @@
               {:fx/type :label
                :text (or description "DEFAULT TEXT")}]})
 
+; endregion
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; region ; COLOR CONTENT
+;
 (defn color-content [{:keys [color] :as params}]
   (println "color-content" params)
   {:fx/type :pane
    :style {:-fx-background-color color}})
 
+; endregion
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; region ; TABLE CONTENT
+;
+(defn- string-cell [{:keys [column/id]}]
+  (fn [x]
+    {:text (id x)}))
+
+(defn- color-cell [_]
+  (fn [x]
+    {:style {:-fx-background-color
+             (:color x)}}))
+
+
+(def reg {:cell/string #'string-cell
+          :cell/color  #'color-cell})
+
+
+(defn- header-cell [{:keys [column/name column/render
+                            column/min-width column/max-width column/pref-width]
+                     :as   cell-heading}]
+  (merge {:fx/type            :table-column
+          :text               name
+          :cell-value-factory identity
+          :cell-factory       {:fx/cell-type :table-cell
+                               :describe     ((render reg) cell-heading)}}
+    (when min-width {:min-width min-width})
+    (when max-width {:max-width max-width})
+    (when pref-width {:pref-width pref-width})))
+
+
+(defn table-view [{:keys [data columns pref-width pref-height]}]
+  {:fx/type     :table-view
+   :pref-width  pref-width
+   :pref-height pref-height
+   :columns     (map header-cell columns)
+   :items       data})
+
+; endregion
